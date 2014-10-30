1ad066a95039e9fc65d3e73261c10db392a6b754
Krystal Flores <krystal.flores@gmail.com>
2014-10-30 13:13:27 -0700

    Linked all navbar pages; data-toggle still not working

---
+++ app/controllers/navbar_controller.rb
@@ -0,0 +1,58 @@
+class NavbarController < ApplicationController
+  def admincreateclasses
+  end
+
+  def admindaily
+	end
+
+	def counselingallstudents
+	end
+
+	def counselingmystudents
+	end
+
+	def counselingresources
+	end
+
+	def hrcreateannouncement
+	end
+
+	def hrpayroll
+	end
+
+	def instructorsmyclasses
+	end
+
+	def instructorsmyschedule
+	end
+
+	def manageadmin
+	end
+
+	def managecounselors
+	end
+
+	def manageinstructors
+	end
+
+	def managehr
+	end
+
+  def managestudents
+  end
+
+  def myhr
+	end
+
+	def statisticsacceptances
+	end
+
+	def statisticstestscores
+	end
+
+	def training
+	end
+
+	def websiteupdates
+	end
+end
